ddf343ce0a063fa71780cbc434adca6fa9e8290f
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-14 21:17:10 +0500

    Created Customer_1 and Vehicle_1 models. - 1 to 1 relationship; - pip install psycopg2-binary;

--- modelingApp/models.py
+++ modelingApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer_1(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Vehicle_1(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='vehicle'
+    )
