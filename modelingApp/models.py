79a61188edb069cf8719d3eddba235589bdfda25
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-15 12:14:04 +0500

    Model Customer_2 and Vehicle_2 are created. - One to many relationship;

--- modelingApp/models.py
+++ modelingApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# 1 to 1 relationship
 class Customer_1(models.Model):
     name = models.CharField(max_length=255)
 
@@ -11,4 +12,18 @@ class Vehicle_1(models.Model):
         Customer_1,
         on_delete=models.CASCADE,
         related_name='vehicle'
+    )
+
+
+# 1 to many relationship
+class Customer_2(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Vehicle_2(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer_2,
+        on_delete=models.CASCADE,
+        related_name='Vehicle'
     )
