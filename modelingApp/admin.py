57bd9f991d7d45721d523185acec6d6666d841c9
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-15 12:22:55 +0500

    Customer_2 and Vehicle_2 models are connected to the project.

--- modelingApp/admin.py
+++ modelingApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Customer_1, Vehicle_1
+from .models import Customer_1, Vehicle_1, Customer_2, Vehicle_2
 
 admin.site.register(Customer_1)
 admin.site.register(Vehicle_1)
+admin.site.register(Customer_2)
+admin.site.register(Vehicle_2)
